8bf057018831738d0c4cb768d94498a4729a1711
Ella Thompson <fang98t@gmail.com>
2020-10-21 00:04:18 -0400

    add css, pseudocode js

---
+++ script.js
@@ -0,0 +1,19 @@
+var city = $().val();
+// api key
+// search button
+// queryurl for city and the api key
+// ajax call (via project 1)
+// ajax temp, conditions
+// append into list (via highscore)
+    // temp, humidity, wind speed, uv index
+    // image ?
+// get the weather now, clear it empty() via project 1
+    // how to calculate weather ?
+// addClass the content & append (via project 1)
+// get 5 day forecast
+    // ajax query + api key
+    // for loop for 5 days
+    // probs a math floor but idk how to do one
+    // temp, humidity
+    // image ?
+    // append
