650e703c7eee8f62ef025e9f074d1333cfad742a
Ella Thompson <fang98t@gmail.com>
2020-10-22 18:31:35 -0400

    fixed locationWeather function, created fiveForecasts function for loop

--- script.js
+++ script.js
@@ -14,10 +14,11 @@ $('#searchBtn').on('click', function() {
         url: queryURL,
         method: 'GET'
     }).then(function(response)  {
+    console.log('response:', response)
 
         cityList(response.name);
         locationWeather(response);
-        fiveForecasts(response);
+        // fiveForecasts(response);
     
     }).catch(function(error)    {
         console.log(error);
@@ -36,43 +37,46 @@ function cityList(city) {
 }
 
 //function to show weather for selected city
-function locationWeather()  {
+function locationWeather(data)  {
     var cardItem = $('<card-title>').text(city);
     console.log(cardItem);
     cardItem.addClass('h4');
     $('#currentCity').empty().append(cardItem);
-    //date //timezone
-    var time = $('<h4>').addClass('card-title').text(data.list[i].weather[0].timezone);
-    console.log(time);
-    time.empty().append(cardItem)
+    //date //timezone //not timezone
+    // var time = $('<h4>').addClass('card-title').text(data.list[i].weather[0].timezone);
+    // console.log(time);
+    // time.empty().append(cardItem)
     //pic //icon
-    var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+    var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
     console.log(img);
-    img.empty().append(time);
+    $('#currentCity').append(img);
     //temp //temp
-    var temp = $("<p>").addClass('card-text').text("Temperature: " + data.list[i].weather[0].temp + 'F');
-    console.log(temp);
-    temp.empty().append(img);
+    var temp = $("<p>").addClass('card-text').text("Temperature: " + data.main.temp + 'F');
+    console.log(temp, 'temp');
+    $('#currentCity').append(temp);
     //humidity //humidity
-    var humidity = $("<p>").addClass('card-text').text('Humidity: ' + data.list[i].weather[0].humidity + '%');
-    console.log(humidity);
-    humidity.empty().append(temp);
+    var humidity = $("<p>").addClass('card-text').text('Humidity: ' + data.main.humidity + '%');
+    console.log(humidity, 'humidity');
+    $('#currentCity').append(humidity);
     //wind speed //speed
-    var wind = $("<p>").addClass('card-text').text("Wind Speed: " + data.list[i].weather[0].speed + "MPH");
+    var wind = $("<p>").addClass('card-text').text("Wind Speed: " + data.wind.speed + "MPH");
     console.log(wind);
-    wind.empty().append(humidity);
+    $('#currentCity').append(wind);
+
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+
     //uv //notsure
-    var queryURL2 = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + {lat} + '&lon=' + {lon} + '&appid=' + city + apiKey;
+    var queryURL2 = 'http://api.openweathermap.org/data/2.5/uvi?' + apiKey + '&lat=' + lat + '&lon=' + lon;
     $.ajax(    {
         url: queryURL2,
         method: 'GET'
-    }).then(function(UV) {
-        console.log(UV);
-        var UV = $("<button>").addClass("btn btn-danger").text("UV Index: " + data.list[i].uvIndex[0].value);
-        UV.empty().append(wind);
-        console.log(uvIndex[0].value);
-    }
-    )};
+    }).then(function(response) {
+        console.log('response:', response)
+        var UV = $("<button>").addClass("btn btn-danger").text("UV Index: " + response.value);
+        $('#currentCity').append(UV);
+    });
+};
       
 //function to show 5 day forecast for selected city
 function fiveForecasts()    {
@@ -82,18 +86,14 @@ function fiveForecasts()    {
         url: queryURL3,
         method: 'GET'
     }).then(function(response)  {
+    console.log('response:', response)
+    forecastItem.empty();
 
-        cityList(response.name);
-        locationWeather(response);
-        fiveForecasts(response);
-    
-    }).catch(function(error)    {
-        console.log(error);
-    });
-
-    var forecastItem = $('<row>').addClass('card col-md-2 ml-4 bg-primary text-white').text(data.list[i].weather[0].timezone)
-    console.log(forecastItem);
-    forecastItem.empty().append('#forecastsAll');
+    for(var i = 0; i < response.length; i++)    {
+        var forecastItem = $('<row>').addClass('card col-md-2 ml-4 bg-primary text-white').text(data.list[i].weather[0].timezone)
+        console.log(forecastItem);
+        forecastItem.append('#forecastsAll');
+    }
 }
 
 //look at bootstrap card in order to figure out the <li> for bootstrap to addClass for 5 forecasts
